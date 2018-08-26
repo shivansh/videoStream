ca8000829705a8c956d2918f418e3e0477224fef
Shivansh Rai <shivansh@freebsd.org>
2018-08-27 04:08:41 +0530

    all: add entry points and remove global declarations

--- client/client.py
+++ client/client.py
@@ -11,19 +11,17 @@ sys.path.insert(0, '../include')
 import helper
 
 args = helper.parser.parse_args()
-frame_count = 0
-socket_ops = 0
 
 
-def cleanup(sock):
+def cleanup(sock, frame_count, socket_ops):
     """Closes the connection and performs cleanup."""
     print 'Closing the socket'
     sock.close()
     cv2.destroyAllWindows()
-    clientStatistics()
+    clientStatistics(frame_count, socket_ops)
 
 
-def clientStatistics():
+def clientStatistics(frame_count, socket_ops):
     """Logs data for tracking client performance."""
     print '\nClient statistics' \
         + '\n-----------------'
@@ -32,71 +30,78 @@ def clientStatistics():
     print ''
 
 
-# Create a TCP/IP socket.
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+def main():
+    frame_count = 0  # number of frames retrieved
+    socket_ops = 0  # number of socket operations (recv) peformed
 
-# Connect the socket to the server on its listening port.
-server_address = (args.server_ip, args.port)
-print 'Connecting to %s:%s' % server_address
-sock.connect(server_address)
+    # Create a TCP/IP socket.
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# Restrict the media-player window size to fit the screen.
-cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+    # Connect the socket to the server on its listening port.
+    server_address = (args.server_ip, args.port)
+    print 'Connecting to %s:%s' % server_address
+    sock.connect(server_address)
 
-try:
-    payload_size = struct.calcsize('Q')
-    data = ""
-    socket_ops = 0
+    # Restrict the media-player window size to fit the screen.
+    cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
 
-    while True:
-        while len(data) < payload_size:
-            payload = sock.recv(helper.chunk_size)
-            if not payload:
-                sys.exit("Server closed the connection.")
-            data += payload
-            socket_ops += 1
+    try:
+        payload_size = struct.calcsize('Q')
+        data = ""
 
-        # Retrieve the payload size by unpacking the first 'payload_size' bytes.
-        packed_frame_dim = data[:payload_size]
-        hashed_frame_dim = struct.unpack('Q', packed_frame_dim)[0]
+        while True:
+            while len(data) < payload_size:
+                payload = sock.recv(helper.chunk_size)
+                if not payload:
+                    sys.exit("Server closed the connection.")
+                data += payload
+                socket_ops += 1
 
-        frame_dims, chunk_size = [], 1
-        while hashed_frame_dim:
-            dim = hashed_frame_dim & 0xFFFF
-            frame_dims += [dim]
-            hashed_frame_dim >>= 16
-            chunk_size *= dim
+            # Retrieve the payload size by unpacking the first 'payload_size' bytes.
+            packed_frame_dim = data[:payload_size]
+            hashed_frame_dim = struct.unpack('Q', packed_frame_dim)[0]
 
-        frame_dims.reverse()
-        data = data[payload_size:]
+            frame_dims, chunk_size = [], 1
+            while hashed_frame_dim:
+                dim = hashed_frame_dim & 0xFFFF
+                frame_dims += [dim]
+                hashed_frame_dim >>= 16
+                chunk_size *= dim
 
-        # Retrieve pending payload (if any).
-        while len(data) < chunk_size:
-            payload = sock.recv(helper.chunk_size)
-            if not payload:
-                sys.exit("Server closed the connection.")
-            data += payload
-            socket_ops += 1
+            frame_dims.reverse()
+            data = data[payload_size:]
 
-        serialized_frame = data[:chunk_size]
+            # Retrieve pending payload (if any).
+            while len(data) < chunk_size:
+                payload = sock.recv(helper.chunk_size)
+                if not payload:
+                    sys.exit("Server closed the connection.")
+                data += payload
+                socket_ops += 1
 
-        # It might be possible that the next payload was retrieved in the above
-        # (second) transfer. Update 'data' to contain only the next payload.
-        data = data[chunk_size:]
+            serialized_frame = data[:chunk_size]
 
-        # Deserialize frames retreived from the payload.
-        frame = np.fromstring(
-            serialized_frame, dtype='uint8').reshape(frame_dims)
-        cv2.imshow('frame', frame)
-        frame_count += 1
-        time.sleep(helper.player_sleep_time)
+            # It might be possible that the next payload was retrieved in the above
+            # (second) transfer. Update 'data' to contain only the next payload.
+            data = data[chunk_size:]
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            # Deserialize frames retreived from the payload.
+            frame = np.fromstring(
+                serialized_frame, dtype='uint8').reshape(frame_dims)
+            cv2.imshow('frame', frame)
+            frame_count += 1
+            time.sleep(helper.player_sleep_time)
 
-except KeyboardInterrupt:
-    sys.exit("Exiting.")
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-finally:
-    cleanup(sock)
+    except KeyboardInterrupt:
+        sys.exit("Exiting.")
+
+    finally:
+        cleanup(sock, frame_count, socket_ops)
+
+
+if __name__ == "__main__":
+    main()
--- server/server.py
+++ server/server.py
@@ -11,41 +11,39 @@ from threading import Thread
 sys.path.insert(0, '../include')
 import helper
 
-# Define the globals
+# Global declarations
 args = helper.parser.parse_args()
-max_concurrent_clients = 5  # Total active clients possible at once.
+max_concurrent_clients = 5  # total active clients possible at once
 
 # TODO Write logic behind this limit.
 max_payload_count = 10 * max_concurrent_clients
 
-# If the reader lags behind the writer by 'lag_threshold'
-# number of payloads, it is synchronized with the writer.
-lag_threshold = 5
-
 # Initialize a constant-sized list of payloads.
 payload_list = [None] * max_payload_count
 
-# Start stream and adjust screen resolution.
-cap = cv2.VideoCapture(0)
-frame_width = 160
-frame_height = 120
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
-
-connection = None
-last_written_index = 0  # Index at which the writer wrote last.
-consumer_thread_count = 0  # Number of active clients.
-payload_count = 0  # Number of payloads sent over network.
+#  connection = None
+last_written_index = 0  # index at which the writer wrote last
+payload_count = 0  # number of payloads sent over network
 
 
-def webcamFeed():
-    """Constructs a payload from the frames collected from
-    the webcam and inserts them into a global list.
+def startWebcam():
+    """Starts the stream and adjusts the screen resolution."""
+    cap = cv2.VideoCapture(0)
+    frame_width = 160
+    frame_height = 120
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+    return cap
+
+
+def webcamFeed(cap):
+    """Constructs a payload from the frames collected from the webcam and
+    inserts them into a global list.
     """
     global payload_list, last_written_index
     payload = ""
-    frame_count = 0
-    write_index = 0
+    frame_count = 0  # number of frames delivered
+    write_index = 0  # most recently updated index of the message queue
 
     while True:
         ret, frame = cap.read()
@@ -62,6 +60,7 @@ def webcamFeed():
 
         # Each payload comprises of 'frames_per_payload' number of the
         # following structures -
+        #
         #       +------------------+---------------+
         #       | Frame dimensions |     Frame     |
         #       |     (Hashed)     | (byte string) |
@@ -82,9 +81,14 @@ def webcamFeed():
             frame_count = 0
 
 
-def handle_connection(connection, client_address, thread_id):
+def handleConnection(connection, client_address, thread_id):
     """Handles an individual client connection."""
     global q, payload_count
+
+    # If the reader lags behind the writer by lag_threshold number of payloads,
+    # it is synchronized with the writer.
+    lag_threshold = 5
+    served_payloads = 0
 
     # The instant when the consumer was created, it should start broadcasting
     # frames which were generated closest to that instant. 'last_written_index'
@@ -97,14 +101,12 @@ def handle_connection(connection, client_address, thread_id):
     if not payload_list[index]:
         index = 0
 
-    # Duration for which consumer waits on the webcam thread to fill up the
-    # queue with payloads.
+    # wait_for_writer represents the duration for which consumer waits on the
+    # webcam thread to fill up the queue with payloads.
     wait_for_writer = helper.frames_per_payload / 10.0
 
     print 'Thread %d: Connection from %s' % (thread_id, client_address)
     print 'Thread %d: Starting broadcast' % thread_id
-
-    served_payloads = 0
 
     try:
         while True:
@@ -114,8 +116,11 @@ def handle_connection(connection, client_address, thread_id):
                     print 'Covered up, waiting for writer'
                 time.sleep(wait_for_writer)
             else:
-                # Evaluate the lag between the reader and the writer.
-                if (last_written_index - index) >= 0:
+                # Evaluate the lag between the reader and the writer. If the
+                # lag is more than the acceptable value, synchronize the reader
+                # by skipping the intermediate payloads and serve the most
+                # recently generated one.
+                if last_written_index >= index:
                     lag = last_written_index - index
                 else:
                     lag = max_payload_count - index + last_written_index
@@ -162,38 +167,50 @@ def serverStatistics():
     print ''
 
 
-# Create a TCP/IP socket.
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+def main():
+    """Sets up a listening socket for incoming connections."""
+    connection = None
+    consumer_thread_count = 0  # number of active clients
 
-# Bind the socket to a port.
-server_address = (args.server_ip, args.port)
-print 'Starting up on %s:%s' % server_address
-sock.bind(server_address)
+    # Create a TCP/IP socket.
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# Listen for incoming connections.
-sock.listen(max_concurrent_clients)
+    # Bind the socket to a port.
+    server_address = (args.server_ip, args.port)
+    print 'Starting up on %s:%s' % server_address
+    sock.bind(server_address)
 
-# Start a thread to collect frames and generate payload to be served to clients.
-webcam_thread = Thread(target=webcamFeed)
-webcam_thread.setDaemon(True)
-webcam_thread.start()
+    # Listen for incoming connections.
+    sock.listen(max_concurrent_clients)
 
-try:
-    while True:
-        print 'Thread %d: Waiting for a connection' % consumer_thread_count
-        connection, client_address = sock.accept()
+    # Start the webcam.
+    cap = startWebcam()
 
-        # Start a consumer thread corresponding to each connected client.
-        consumer_thread = Thread(
-            target=handle_connection,
-            args=(connection, client_address, consumer_thread_count))
-        consumer_thread_count += 1
-        consumer_thread.setDaemon(True)
-        consumer_thread.start()
+    # Start a thread to collect frames and generate payload to be served to clients.
+    webcam_thread = Thread(target=webcamFeed, args=(cap,))
+    webcam_thread.setDaemon(True)
+    webcam_thread.start()
 
-except KeyboardInterrupt:
-    sys.exit("Exiting.")
+    try:
+        while True:
+            print 'Thread %d: Waiting for a connection' % consumer_thread_count
+            connection, client_address = sock.accept()
 
-finally:
-    cleanup(connection)
+            # Start a consumer thread corresponding to each connected client.
+            consumer_thread = Thread(
+                target=handleConnection,
+                args=(connection, client_address, consumer_thread_count))
+            consumer_thread_count += 1
+            consumer_thread.setDaemon(True)
+            consumer_thread.start()
+
+    except KeyboardInterrupt:
+        sys.exit("Exiting.")
+
+    finally:
+        cleanup(connection)
+
+
+if __name__ == "__main__":
+    main()
